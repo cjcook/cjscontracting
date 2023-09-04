2dbc231118adac5ba67430d14c731e887280932c
Chris Cook <chriscook81798@gmail.com>
2023-09-04 12:27:21 -0400

    Updated contact info on website

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "l.krentz@greatlakes-hvac.com";
+    $to = "cj@cjscontracting.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
